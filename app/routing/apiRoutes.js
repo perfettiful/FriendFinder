dfff342cab3d5e1045ff6fa58fb76f962cba94a3
Nate <nathanperfetti@gmail.com>
2018-06-13 17:10:08 -0500

    Added both POST and GET routes on /api/firends endpt

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,25 @@
+var friendsData = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+
+  app.post("/api/friends", function (req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+      tableData.push(req.body);
+  });
+};
+  app.get("/api/friends", function (req, res) {
+    res.json(waitListData);
+  });
+
