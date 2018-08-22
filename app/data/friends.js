22ff28c5f80044a638dbce08f58a0c50100f1a6d
Nate <nathanperfetti@gmail.com>
2018-08-21 19:14:40 -0500

    Finished friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,60 @@
-var ahmed = {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  };
+var friends = [
+    {
+        "name": "Charlie Brown",
+        "photo": "https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-0.3.5&s=f7007b5e33474e6a9a02403d0db403d9&auto=format&fit=crop&w=2250&q=80",
+        "scores": ["5", "1","4","4","5","1","2","5","4","1"]
+    },
+    {
+        "name": "Peppermint Patty",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": ["1","4","6","3","2","3","1","1","1","3"]
 
-var friendsData = [ahmed];
+    },
+    {
+        "name": "Lucy",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": ["2","6","4","9","2","3","1","1","1","3"]
+    },
+    {
+        "name": "Snoopy",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": ["3","4","4","8","2","3","1","1","1","3"]
+    },
+    {
+        "name": "Sally",
+        "photo": "https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6138d4bb4d3515092f3379fb64fbbb00&auto=format&fit=crop&w=2250&q=80",
+        "scores": ["5","4","4","8","2","3","1","1","6","3"]
+    },
+    {
+        "name": "Linus",
+        "photo": "https://images.unsplash.com/photo-1440589473619-3cde28941638?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42ebdb92a644e864e032a2ebccaa25b6&auto=format&fit=crop&w=934&q=80",
+        "scores": ["6","4","4","7","2","6","1","1","1","3"]
+    },
+    {
+        "name": "Woodstock",
+        "photo": "https://images.unsplash.com/photo-1458724338480-79bc7a8352e4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7b54da7a4d4a137d0272c5de7ed68d54&auto=format&fit=crop&w=2250&q=80",
+        "scores": ["7","4","4","6","6","3","1","1","1","3"]
+    },
+    {
+        "name": "Schroeder",
+        "photo": "https://www.fillmurray.com/640/360",
+        "scores": ["8","2","4","5","2","3","1","1","1","3"]
+    }
+    ,{
+        "name": "Pigpen",
+        "photo": "https://www.fillmurray.com/640/360",
+        "scores": ["9","3","4","4","2","3","1","1","1","3"]
+    }
+    ,{
+        "name": "Marcie",
+        "photo": "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/marcie-snoopys-getting-married-charlie-brown-8.72.jpg",
+        "scores": ["1","2","4","2","2","3","1","9","1","3"]
+    }
+    ,{
+        "name": "Franklin",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/9/9d/Franklin_%28Peanuts%29.png",
+        "scores": ["3","4","2","1","2","3","1","6","1","3"]
+    }
+];//End friendsData array
 
 module.exports = friendsData;
