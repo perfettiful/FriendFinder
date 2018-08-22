1efd6c26c96ee870391ba042ee093fda7a99f803
Nate <nathanperfetti@gmail.com>
2018-08-22 00:09:27 -0500

    Updated routes and form action

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,8 +3,6 @@ var friendsData = require("../data/friends");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-
-module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
@@ -12,14 +10,40 @@ module.exports = function (app) {
   // ---------------------------------------------------------------------------
 
 
-  app.post("/api/friends", function (req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-      tableData.push(req.body);
-  });
-};
-  app.get("/api/friends", function (req, res) {
-    res.json(waitListData);
-  });
+  module.exports = function (app) {
+    // API GET Requests to send the friends JSON object
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
 
+    // POST , when requested submitted to post, push the new object to array and respond back as true;
+    app.post("/api/friends", function (req, res) {
+
+        var input = req.body
+
+        //set empty array to calcuate the difference for all friends in existing array items
+        var diffArray = [];
+
+        // for each friend object in the array, calculaate the diffrence of each score item and sum that up
+        friendsData.forEach(obj => {
+            var difference = 0;
+            obj.scores.forEach((item, index) => {
+                var friendscrore = parseInt(item);
+                var userscrore = parseInt(input.scores[index]);
+                difference += Math.abs(friendscrore - userscrore);
+            });
+            diffArray.push(difference);
+        });
+
+        // Get the least value of the difference array and corresponding index value of the friends Array
+        var output = friendsData[diffArray.indexOf(Math.min(...diffArray))]
+
+        //Now push the request to array
+        friendsData.push(input);
+
+        // send the response back to POST request
+        res.json(output);
+
+    });
+
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,12 @@ module.exports = function (app) {
 
         app.get("/survey", function (req, res) {
             res.sendFile(path.join(__dirname, "../public/survey.html"));
+       
         });
 
-}
+          // If no matching route is found - redirect to home - API route is already taken care by precedence.
+          app.get("*", function (req, res) {
+              res.sendFile(path.join(__dirname, "../public/home.html"));
+          });
+
+};
