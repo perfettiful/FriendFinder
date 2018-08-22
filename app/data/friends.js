c970eff86c4d4955cab02dace7070aac1f61e35b
Nate <nathanperfetti@gmail.com>
2018-08-21 19:22:19 -0500

    Image links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,48 +1,48 @@
 var friends = [
     {
         "name": "Charlie Brown",
-        "photo": "https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-0.3.5&s=f7007b5e33474e6a9a02403d0db403d9&auto=format&fit=crop&w=2250&q=80",
+        "photo": "https://vignette.wikia.nocookie.net/peanuts/images/e/e2/Charlie_Brown_-_AAUGH%21.jpg/revision/20131130072535",
         "scores": ["5", "1","4","4","5","1","2","5","4","1"]
     },
     {
         "name": "Peppermint Patty",
-        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/peanuts/images/a/a0/Peppermint_Patty.png/revision/latest?cb=20160715071317",
         "scores": ["1","4","6","3","2","3","1","1","1","3"]
 
     },
     {
         "name": "Lucy",
-        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Lucy_van_Pelt.png/220px-Lucy_van_Pelt.png",
         "scores": ["2","6","4","9","2","3","1","1","1","3"]
     },
     {
         "name": "Snoopy",
-        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/peanuts/images/a/a0/Sally-0.jpg/revision/latest?cb=20160619080818",
         "scores": ["3","4","4","8","2","3","1","1","1","3"]
     },
     {
-        "name": "Sally",
+        "name": "Sally Brown",
         "photo": "https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6138d4bb4d3515092f3379fb64fbbb00&auto=format&fit=crop&w=2250&q=80",
         "scores": ["5","4","4","8","2","3","1","1","6","3"]
     },
     {
-        "name": "Linus",
-        "photo": "https://images.unsplash.com/photo-1440589473619-3cde28941638?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42ebdb92a644e864e032a2ebccaa25b6&auto=format&fit=crop&w=934&q=80",
+        "name": "Linus van Pelt",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Linus_van_Pelt.gif/220px-Linus_van_Pelt.gif",
         "scores": ["6","4","4","7","2","6","1","1","1","3"]
     },
     {
         "name": "Woodstock",
-        "photo": "https://images.unsplash.com/photo-1458724338480-79bc7a8352e4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7b54da7a4d4a137d0272c5de7ed68d54&auto=format&fit=crop&w=2250&q=80",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Woodstock.png/220px-Woodstock.png",
         "scores": ["7","4","4","6","6","3","1","1","1","3"]
     },
     {
         "name": "Schroeder",
-        "photo": "https://www.fillmurray.com/640/360",
+        "photo": "https://vignette.wikia.nocookie.net/peanuts/images/8/87/Schroeder.jpg/revision/latest?cb=20090301042650",
         "scores": ["8","2","4","5","2","3","1","1","1","3"]
     }
     ,{
         "name": "Pigpen",
-        "photo": "https://www.fillmurray.com/640/360",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/PigPen_%28Peanuts%29.png/220px-PigPen_%28Peanuts%29.png",
         "scores": ["9","3","4","4","2","3","1","1","1","3"]
     }
     ,{
